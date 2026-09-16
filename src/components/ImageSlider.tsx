45e6eabc388502b73efffd444c6c9ef345d59527
puneerkumarboinpally <109670302+Puneetkumarboinpally@users.noreply.github.com>
2026-09-16 21:41:26 +0100

    slide functionality added

--- src/components/ImageSlider.tsx
+++ src/components/ImageSlider.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import heroMovies from "../api/tmdb";
 import type { Movie } from "../types/movie";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 
 const ImageSlider = () => {
   const [bannerMovies, setBannerMovies] = useState<Movie[]>([]);
+  const [currentSlide, setCurrentSlide] = useState(0);
 
   useEffect(() => {
     heroMovies().then((data) => {
@@ -12,17 +14,45 @@ const ImageSlider = () => {
     });
   }, []);
 
+  const prevSlide = () => {
+    setCurrentSlide((prev) =>
+      prev === 0 ? bannerMovies.length - 1 : prev - 1,
+    );
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) =>
+      prev === bannerMovies.length - 1 ? 0 : prev + 1,
+    );
+  };
+
   return (
-    <div className="flex overflow-hidden">
-      {bannerMovies.map((movie) => (
-        <div key={movie.id} className="min-w-full">
-          <img
-            src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
-            alt={movie.title}
-            className="h-full w-full object-cover"
-          />
-        </div>
-      ))}
+    <div className="relative h-screen overflow-hidden">
+      <div
+        className="flex transition-transform duration-500 ease-out"
+        style={{ transform: `translateX(-${currentSlide * 100}%)` }}
+      >
+        {bannerMovies.map((movie) => (
+          <div key={movie.id} className="h-full w-full shrink-0">
+            <div className="relative">
+              <img
+                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                alt={movie.title}
+                className="h-full w-full object-cover"
+              />
+              <div></div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <div className="absolute bottom-5 left-1/2 -translate-x-1/2">
+        <button onClick={prevSlide}>
+          <ChevronLeft />
+        </button>
+        <button onClick={nextSlide}>
+          <ChevronRight />
+        </button>
+      </div>
     </div>
   );
 };
